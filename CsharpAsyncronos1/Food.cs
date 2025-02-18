9039ca4cedb9806de0177977da8f335bda3f9530
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-18 22:26:39 +0100

    begin

---
+++ CsharpAsyncronos1/Food.cs
@@ -0,0 +1,24 @@
+﻿namespace CsharpAsyncronos1;
+
+public abstract class Food
+{
+    private readonly TimeSpan _cookTime;
+    public string Name { get; }
+
+    protected Food(TimeSpan cookTime)
+    {
+        _cookTime = cookTime;
+        Name = GetType().Name;
+    }
+
+    public async Task Cook()
+    {
+        Console.WriteLine($"Cooking food {Name}");
+        await Task.Delay(_cookTime);
+        Console.WriteLine("Completed cooking food");
+    }
+}
+public class Turkey() : Food(TimeSpan.FromSeconds(5));
+public class MashedPotato() : Food(TimeSpan.FromSeconds(2));
+public class Gravy() : Food(TimeSpan.FromSeconds(5));
+public class Stuffy() : Food(TimeSpan.FromSeconds(2));
---
+++ CsharpAsyncronos1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CsharpAsyncronos1;
+
+Console.WriteLine("Cooking started... ");
+var turkey = new Turkey();
+await turkey.Cook();
+Console.WriteLine("cooking turkey is finished");
+var grovy = new Gravy();
+await grovy.Cook();
+Console.WriteLine("ready to eat");
